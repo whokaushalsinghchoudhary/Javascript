3fce96239a0501b3fdffe3693356969bf25dd7ff
whokaushalsinghchoudhary <138804392+whokaushalsinghchoudhary@users.noreply.github.com>
2024-04-13 06:32:30 +0000

    config done

---
+++ basic/datatypes.js
@@ -0,0 +1,18 @@
+let name = "Kaushal" //String Datatype
+let age = 22 // Integer
+let isLoggedIn = false // boolean
+
+// number 
+// bigInt
+// String => ""
+// boolean => TRUE OR False
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+
+// Object
+
+
+console.log(typeof "Kaushal");
